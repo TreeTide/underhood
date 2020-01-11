486a1aaeb98104cd6d271ffbcf790ff6de02da50
Robin Palotai <palotai.robin@gmail.com>
2020-01-12 00:37:34 +0100

    Add missing file.

---
+++ treetide/underhood/ui/src/FileName.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <LangIcon :for-file="filePath" /> {{filePath}}
+  </div>
+</template>
+
+<script>
+
+import LangIcon from './LangIcon.vue';
+
+export default {
+  components: {
+    LangIcon,
+  },
+  props: {
+    filePath: String,
+  },
+}
+</script>
+
+<style scoped>
+</style>
